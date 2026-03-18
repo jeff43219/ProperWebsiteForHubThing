b74624c5a4839b9e684503fff4e77bdcdb717696
jeff43219 <jeffishim43@protonmail.com>
2026-03-18 21:52:25 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path' // You might need to install this if it's missing
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // This tells Vite: "@" means the "src" folder
       '@': path.resolve(__dirname, './src'),
     },
   },
